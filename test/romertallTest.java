5df94717a7913347a75dbee89256b3958181c7af
SamuelEBG <95290084+SamuelEBG@users.noreply.github.com>
2022-09-01 14:20:16 +0200

    :large_blue_circle: fixed 4, 9, 40, 90, 400, 900

--- test/romertallTest.java
+++ test/romertallTest.java
@@ -16,6 +16,41 @@ class romertallTest {
     }
 
     @Test
+    void testNumberSix(){
+        assertEquals("IX", toRoman(9));
+    }
+
+    @Test
+    void trettioatta(){
+        assertEquals("XXXVIII", toRoman(38));
+    }
+
+    @Test
+    void test789(){
+        assertEquals("DCCLXXXIX", toRoman(789));
+    }
+
+    @Test
+    void test2421(){
+        assertEquals("MMCDXXI", toRoman(2421));
+    }
+
+    @Test
+    void testNumber19(){
+        assertEquals("XIX", toRoman(19));
+    }
+
+    @Test
+    void test246(){
+        assertEquals("CCXLVI", toRoman(246));
+    }
+
+    @Test
+    void test93(){
+        assertEquals("XCIII", toRoman(93));
+    }
+
+    @Test
     void testNegativeNumber(){
         assertEquals("Not a Roman number!", toRoman(-1));
         assertEquals("Not a Roman number!", toRoman(-9999));
@@ -29,60 +64,83 @@ class romertallTest {
 
     private String toRoman(int number) {
 
-
         if((number == 0) || (number < 0)){
             return "Not a Roman number!";
         }
 
         int num = number;
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         do {
 
-            if (num == 4){
-                num -= 4;
-                result += "IV";
-            }
-
             if(num < 5){
-                num -= 1;
-                result += "I";
+                if (num == 4){
+                    num -= 4;
+                    result.append("IV");
+
+                } else{
+                    num -= 1;
+                    result.append("I");
+                }
             }
 
             if(num < 10 && num >= 5){
-                num -= 5;
-                result += "V";
+                if(num == 9){
+                    num -= 9;
+                    result.append("IX");
+                } else{
+                    num -= 5;
+                    result.append("V");
+                }
             }
 
             if(num < 50 && num >= 10){
-                num -= 10;
-                result += "X";
+
+                if(num >= 40){
+                    num -= 40;
+                    result.append("XL");
+                } else{
+                    num -= 10;
+                    result.append("X");
+                }
             }
 
-            if(num < 100 && num >=50){
-                num -= 50;
-                result += "L";
+            if(num < 100 && num >= 50){
+                if(num >= 90){
+                    num -= 90;
+                    result.append("XC");
+                } else{
+                    num -= 50;
+                    result.append("L");
+                }
             }
-
 
             if(num < 500 && num >= 100){
-                num -= 100;
-                result += "C";
+                if(num >= 400){
+                    num -= 400;
+                    result.append("CD");
+                } else{
+                    num -= 100;
+                    result.append("C");
+                }
             }
 
             if (num < 1000 && num >= 500){
+                if(num >= 900){
+                    num -= 900;
+                    result.append("CM");
+                }
                 num -= 500;
-                result += "D";
-
+                result.append("D");
             }
 
             if (num >= 1000) {
                 num -= 1000;
-                result += "M";
+                result.append("M");
             }
 
         } while (num > 0);
 
-        return result;
+        return result.toString();
     }
 }
